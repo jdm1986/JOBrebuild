bc6a31a570c0e644c3c234c0082f4e3e569fbb23
Josh Mix <joshuadmix@gmail.com>
2018-04-19 15:59:09 -0400

    improved slider photos

--- wp-content/plugins/simple-sticky-footer/simple-sticky-footer.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    if (typeof delay !== 'undefined') {
-        setTimeout(function(){
-            jQuery('#simple-sticky-footer-container').effect( effect, 500 );
-        },delay);
-    }
-});  
