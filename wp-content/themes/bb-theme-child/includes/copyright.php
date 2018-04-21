3546f0f9ce92ba9a9fc3dc94e0a1090e1694f122
Josh Mix <joshuadmix@gmail.com>
2018-04-21 18:47:51 -0400

    corrected footer sitewide

---
+++ wp-content/themes/bb-theme-child/includes/copyright.php
@@ -0,0 +1,5 @@
+<div class="fl-page-footer-text fl-page-footer-text-1">
+	<span>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></span>
+	<span> | </span>
+	<span id="fl-site-credits" style="color: white;"> 6104 Sable Mill Court | Jeffersonville, IN 47130 | (812) 585-3094 | <a href="mailto:jmix@jobrentals.com">jmix@jobrentals.com</a> </span>
+</div>
